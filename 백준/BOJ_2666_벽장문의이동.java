3540db1879a9f4eac7e2a7d70ed5be447f0d236c
leejk118 <leejk118@gmail.com>
2019-06-14 23:51:40 +0900

    20190614-1

---
+++ 백준/BOJ_2666_벽장문의이동.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static int n;
+    public static int[] target;
+    public static int answer = Integer.MAX_VALUE;
+     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+        int num = sc.nextInt();
+        target = new int [num];
+        for (int i = 0; i < num; ++i) {
+            target[i] = sc.nextInt();
+        }
+        dfs(0, 0, first, second);
+        System.out.println(answer);
+        sc.close();
+    }
+     
+    public static void dfs(int count, int cost, int first, int second) {
+        if (count == target.length) {
+            if (answer > cost)
+                answer = cost;
+            return ;
+        }
+        if (target[count] == first || target[count] == second)
+            dfs(count + 1, cost, first, second);
+        else if (target[count] < first) 
+            dfs(count + 1, cost + (first - target[count]), target[count], second);
+        else if (second < target[count])
+            dfs(count + 1, cost + (target[count] - second), first, target[count]);
+        else {
+            dfs(count + 1, cost + (target[count] - first), target[count], second);
+            dfs(count + 1, cost + (second - target[count]), first, target[count]);
+        }
+    }
+}

4ac7a49a7775e5925989e978d23ba17481d7658e
leejk118 <leejk118@gmail.com>
2019-06-27 23:28:53 +0900

    20190627-1

---
+++ 백준/BOJ_1520_내리막길.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1520_내리막길 {
+
+	public static int N, M;
+	public static int[][] map, dp;
+	public static int[] dx = {-1, 0, 1, 0};
+	public static int[] dy = {0, -1, 0, 1};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		dp = new int[N][M];
+		
+		for (int i = 0; i < N; ++i) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; ++j) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				dp[i][j] = -1;
+			}
+		}
+		System.out.println(dfs(0, 0));
+	}
+	
+	public static int dfs(int x, int y) {
+		if (dp[x][y] != -1) return dp[x][y];
+		if (x == N - 1 && y == M - 1) return 1;
+		dp[x][y] = 0;
+		for (int i = 0; i < 4; ++i) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (0 <= nx && nx < N && 0 <= ny && ny < M) {
+				if (map[x][y] > map[nx][ny]) {
+					dp[x][y] += dfs(nx, ny);
+				}
+			}
+		}
+		return dp[x][y];
+	}
+
+}
